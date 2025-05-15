02ff96d13703f3a7a1aa1c440c9cd2cca1984337
nuxlox <100982284+nuxl0x@users.noreply.github.com>
2025-05-15 17:30:34 +1000

    Implement basic functionality within vos.sh
    Basically just added the case code for finding the command and running the dedicated script + passing on the arguments. This code will change whenever new commands are added, and there will also be color coding soon.

--- vos.sh
+++ vos.sh
@@ -1,1 +1,57 @@
 #!/usr/bin/bash
+
+# script variable statements
+COMMAND="$1"
+ARG_1="$2"
+ARG_2="$3"
+ARG_3="$4"
+
+COMMANDS_DIRECTORY="/usr/local/share/vos/commands"
+ASSETS_DIRECTORY="/usr/local/share/vos/assets"
+
+# command functions (functions that are called upon determining the correct command)
+
+function cmdlist {
+    bash "$COMMANDS_DIRECTORY/cmdlist.sh"
+}
+
+function install_dependencies {
+    bash "$COMMANDS_DIRECTORY/install_dependencies.sh"
+}
+
+function man {
+    bash "$COMMANDS_DIRECTORY/man.sh" "$ARG_1"
+}
+
+function math {
+    bash "$COMMANDS_DIRECTORY/math.sh" "$ARG_1" "$ARG_2" "$ARG_3"
+}
+
+function scan {
+    bash "$COMMANDS_DIRECTORY/scan.sh" "$ARG_1" # potentially pending more arguments
+}
+
+function uninstall {
+    bash "$COMMANDS_DIRECTORY/uninstall.sh"
+}
+
+function help {
+    echo "Usage: vos <command> [args...]"
+    echo "Run 'vos cmdlist' to view all commands."
+}
+
+case "$COMMAND" in
+    cmdlist) cmdlist ;;
+    install-dependencies) install_dependencies ;;
+    man) man ;;
+    math) math ;;
+    scan) scan ;;
+    uninstall) uninstall ;;
+    ""|help) help ;;
+    *)
+        echo "Error: Command '$COMMAND' not found."
+        echo "If you believe this to be an issue, report this error on Github."
+        echo
+        help
+        exit 1
+esac
