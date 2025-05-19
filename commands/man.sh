5fb6c473781e76919656df9380b28caa11c319a0
nuxlox <100982284+nuxl0x@users.noreply.github.com>
2025-05-19 14:20:25 +1000

    Update man.sh

--- commands/man.sh
+++ commands/man.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/bash
+
+ASSETS="/usr/local/share/vos/assets"
+COMMAND="$1"
+
+case "$COMMAND" in
+  cmdlist) cat "$ASSETS/man_cmdlist.txt" ;;
+  install-dependencies) cat "$ASSETS/man_install_dependencies.txt" ;;
+  man) cat "$ASSETS/man_man.txt" ;;
+  math) cat "$ASSETS/man_math.txt" ;;
+  uninstall) cat "$ASSETS/man_uninstall.txt" ;;
+esac
