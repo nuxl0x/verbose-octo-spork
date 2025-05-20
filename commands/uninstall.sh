290fc87b230097688255cd5409a8120255b2f05f
nuxlox <100982284+nuxl0x@users.noreply.github.com>
2025-05-20 12:25:48 +1000

    uninstall.sh | Primary functionality.

--- commands/uninstall.sh
+++ commands/uninstall.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
 
 function uninstall {
-  
+  rm "/usr/local/bin/vos"
+  rm -rf "/usr/local/share/vos"
 }
 
 echo "[VOS-WARNING] This command will COMPLETELY delete VOS and all related files off of your computer."
+read -p "[VOS-INPUT] Confirm that you wish to continue? (y/n): " uninstall_confirm
+uninstall_confirm=${$uninstall_confirm,,}
+if [[ "$uninstall_confirm" == "y" ]]; then
+  uninstall
+fi
+
+echo "[VOS-ERROR] Uninstallation not confirmed. Exiting..."
+exit 1
