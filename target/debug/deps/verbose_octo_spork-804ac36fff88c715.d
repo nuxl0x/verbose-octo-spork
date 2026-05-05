33aa9f563bd1e08b6ed3d0c272cd4ae8061b8d27
nuxl0x <nuclearx86@gmail.com>
2026-05-05 12:19:25 +1000

    Basic Rust Structure.

---
+++ target/debug/deps/verbose_octo_spork-804ac36fff88c715.d
@@ -0,0 +1,5 @@
+C:\Users\pri0031\RustroverProjects\verbose-octo-spork\target\debug\deps\verbose_octo_spork-804ac36fff88c715.d: src\main.rs
+
+C:\Users\pri0031\RustroverProjects\verbose-octo-spork\target\debug\deps\libverbose_octo_spork-804ac36fff88c715.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/verbose_octo_spork-d1abb1d95ea123d3.d
@@ -0,0 +1,5 @@
+C:\Users\pri0031\RustroverProjects\verbose-octo-spork\target\debug\deps\verbose_octo_spork-d1abb1d95ea123d3.d: src\main.rs
+
+C:\Users\pri0031\RustroverProjects\verbose-octo-spork\target\debug\deps\libverbose_octo_spork-d1abb1d95ea123d3.rmeta: src\main.rs
+
+src\main.rs:
